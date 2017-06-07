45b847eaa2b30962d04ca3a6787358dc9e66367c
v.sasikumar <sasikumar0114@gmail.com>
2017-06-07 14:21:07 +0530

    Add files via upload

---
+++ ass15.java
@@ -0,0 +1,17 @@
+package pra;
+
+import java.util.Scanner;
+
+public class ass15 {
+	public static void main(String[] args) {
+		System.out.println("Enter the interval reange:");
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a;i<=b;++i){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+	}
+}
